38a0bef0b9b6233dbf1a97acb8d36d361f2b8e90
sovietor <94129991+heygdrg@users.noreply.github.com>
2022-06-30 10:30:18 +0200

    Add files via upload

--- DOWNLOADER.py
+++ DOWNLOADER.py
@@ -1,5 +1,4 @@
 import os
-
 from pystyle import *
 from pytube import YouTube
 import webbrowser
@@ -14,22 +13,22 @@ banner = """
     $$ |  $$ |$$ |  $$ |$$$  / \$$$ |$$ |\$$$ |$$ |     $$ |  $$ |$$ |  $$ |$$ |  $$ |$$ |      $$ |  $$ |
     $$$$$$$  | $$$$$$  |$$  /   \$$ |$$ | \$$ |$$$$$$$$\ $$$$$$  |$$ |  $$ |$$$$$$$  |$$$$$$$$\ $$ |  $$ |
     \_______/  \______/ \__/     \__|\__|  \__|\________|\______/ \__|  \__|\_______/ \________|\__|  \__|
-                                        
-                                            >BKS#1958<                                                                     
-                                >>> https://discord.gg/6eAAKVuw <<<                                                         
-                       
-                            ╔═════════════════════════════════════════════╗
-                            [1] YOUTUBE DOWNLOADER
-                            [2] INSTAGRAM DOWNLOADER
-                            [3] SPOTIFY DOWNLOADER [coming soon]
-                            [4] EXIT
-                            ╚═════════════════════════════════════════════╝
+                                                                                                      
+                                                                                                      
+                        ╔═════════════════════════════════════════════╗
+                        [1] YOUTUBE DOWNLOADER
+                        [2] INSTAGRAM DOWNLOADER
+                        [3] QR CODE GENERATOR [COMING SOON]
+                        [4] SPOTIFY DOWNLOADER [COMING SOON]
+                        [5] EXIT
+                        ╚═════════════════════════════════════════════╝
 """
 
 print(Colorate.Horizontal(Colors.yellow_to_red, Center.XCenter(banner), 1))
 
 print()
-choice = input(">>> choice : ")
+choice = Write.Input(">>> choice : ", Colors.yellow_to_red, interval=0.001)
+
 
 if choice == "1":
 
@@ -39,13 +38,13 @@ if choice == "1":
 
     a = 0
     while a == 0:
-        link = input("enter the link: ")
+        link = Write.Input("link: ", Colors.yellow_to_red, interval=0.001)
         yt = YouTube(link)
 
         video = yt.streams.get_highest_resolution()
         video.download()
         print("Done!")
-        again = input("wanna download another video [y/n] :")
+        again =  Write.Input("wanna download another video [y/n] :", Colors.yellow_to_red, interval=0.001)
         if again == "y":
             a = 0
         if again == "n":
@@ -53,24 +52,36 @@ if choice == "1":
 
 
 if choice == "2":
+
+    webbrowser.open("https://www.instagram.com/")
+    print()
+    print("We just open instagram, easier to have a link !")
+
     a = 0
     while a == 0:
         ig = instaloader.Instaloader()
-        dp = input("Enter Insta Username : ")
+        dp = Write.Input("Enter Insta Username : ", Colors.yellow_to_red, interval=0.001)
         ig.download_profile(dp, profile_pic_only=True)
         print("Done!")
-        again = input("wanna download another video [y/n] :")
+        again = Write.Input("wanna download another video [y/n] :", Colors.yellow_to_red, interval=0.001)
         if again == "y":
             a = 0
         if again == "n":
             a = 1
 
-if choice == "3":
 
-    print("coming soon")
-    #song = input("enter the URL: ")
-    #os.system(f" spotdl {song}")
-    #os.remove(".spotdl-cache")
+if choice == "3":
+    Write.Input("[COMING SOON]", Colors.yellow_to_red, interval=0.001)
+
+#link = input("link: ")
+#img = qrcode.make(link)
+#ing.show()
+#song = input("enter the URL: ")
+#os.system(f" spotdl {song}")
+#os.remove(".spotdl-cache")
 
 if choice == "4":
+    Write.Input("[COMING SOON]", Colors.yellow_to_red, interval=0.001)
+
+if choice == "5":
     os.system("PAUSE")
